aa75030735bc3ac9cb1e7f9085ac21d6d14436c1
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2017-07-19 23:51:16 -0300

    criadas tabelas de dependentes e quartos

--- ddl_grupo09.sql
+++ ddl_grupo09.sql
@@ -1,9 +1,12 @@
-DROP TABLE IF EXISTS Clientes;
+DROP TABLE IF EXISTS Quartos;
+DROP TABLE IF EXISTS Dependentes;
 DROP TABLE IF EXISTS Telefones;
+DROP TABLE IF EXISTS Clientes;
+
 
 CREATE TABLE IF NOT EXISTS Clientes (
-	cpf char(11) PRIMARY KEY NOT NULL,
 	nome varchar(200) NOT NULL,
+    cpf char(11) PRIMARY KEY NOT NULL,
 	dataNascimento DATE NOT NULL,
 	sexo char(1) NOT NULL,
 	rua varchar(200) NOT NULL, 
@@ -15,22 +18,29 @@ CREATE TABLE IF NOT EXISTS Clientes (
 CREATE TABLE IF NOT EXISTS Telefones (
 	cpf char(11) NOT NULL,
 	ddd char(3) NOT NULL,
-	telefone varchar(9) NOT NULL,
-	
-    PRIMARY KEY (ddd, telefone),
-    CONSTRAINT cliente_telefone
-		FOREIGN KEY (cpf) REFERENCES Clientes(cpf) ON DELETE CASCADE
-) ENGINE INNODB;
+	telefone varchar(9) NOT NULL,	
+    
+    PRIMARY KEY (ddd, telefone)
+) DEFAULT CHARSET=utf8, ENGINE INNODB;
 
--- INSERT INTO Clientes VALUES('11445733439', 
---                               'Lucas Henrique',
---                               '1997-04-04',
---                               'M', 
---                               'Rua 1', 
---                               '1400', 
---                               'Universitário',
---                               '55750000');
--- 
-INSERT INTO Telefones VALUES(
-	'11445733439', '081', '9999999'
-);
+alter table Telefones add constraint cpf 
+	foreign key (cpf) references Clientes (cpf) on delete cascade;
+
+create table if not exists Dependentes (
+	nome varchar(200) not null,
+    cpf_cliente char(11) not null,
+    dataNascimento Date not null,
+    
+    primary key (nome, dataNascimento)
+) DEFAULT CHARSET=utf8, ENGINE INNODB;
+
+alter table Dependentes add constraint cpf_cliente
+	foreign key (cpf_cliente) references Clientes (cpf) on delete cascade;
+    
+create table if not exists Quartos (
+	id int not null primary key,
+    numero char(3) unique,
+    tipo varchar(100),
+    vista boolean,
+    diaria real
+) default charset=utf8, engine innodb;
---
+++ testes.sql
@@ -0,0 +1,14 @@
+ INSERT INTO Clientes VALUES('Lucas Henrique', '11445733439', '1997-04-04', 'M', 
+'Rua 1','1400','Universitário','55750000');
+
+ INSERT INTO Telefones VALUES(
+	'11445733439','081', '9999999'    
+);
+
+insert into Dependentes values(
+	'Carla', '11445733439', '1995-05-14'
+);
+
+insert into Quartos values(
+	123, '001', 'normal', false, 50.00
+);

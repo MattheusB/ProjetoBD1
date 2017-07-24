12242e160e0df946fc746262a4f25cc4e0aa15a3
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2017-07-24 08:42:09 -0300

    Add new tables

--- ddl_grupo09.sql
+++ ddl_grupo09.sql
@@ -1,7 +1,14 @@
+DROP TABLE IF EXISTS Hospeda;
+DROP TABLE IF EXISTS Reserva;
 DROP TABLE IF EXISTS Telefones;
 DROP TABLE IF EXISTS Dependente;
 DROP TABLE IF EXISTS Nota_Fiscal;
 DROP TABLE IF EXISTS Clientes;
+DROP TABLE IF EXISTS Equipamento;
+DROP TABLE IF EXISTS Quarto;
+DROP TABLE IF EXISTS Funcionario;
+DROP TABLE IF EXISTS Servico;
+DROP TABLE IF EXISTS Produto;
 
 CREATE TABLE IF NOT EXISTS Clientes (
 	cpf char(11) PRIMARY KEY NOT NULL,
@@ -24,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Telefones (
 		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE
 ) DEFAULT CHARSET=utf8;
 
-CREATE TABLE Dependente(
+CREATE TABLE IF NOT EXISTS Dependente(
     cpf varchar(11) NOT NULL,
     cpfCli varchar(11) NOT NULL,
 	nome varchar(200) NOT NULL,
@@ -35,7 +42,7 @@ CREATE TABLE Dependente(
 		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE
 ) DEFAULT CHARSET=utf8;
 
-CREATE TABLE Nota_Fiscal(
+CREATE TABLE IF NOT EXISTS Nota_Fiscal(
 	id_nota INT NOT NULL,
     cpfCli varchar(11) NOT NULL,
     dt_in DATE NOT NULL,
@@ -45,4 +52,73 @@ CREATE TABLE Nota_Fiscal(
     PRIMARY KEY (id_nota),
     CONSTRAINT cpfCliente
 		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Quarto (
+	id INT NOT NULL,
+    numero INT NOT NULL UNIQUE,
+    tipo varchar(100) NOT NULL,
+    vista boolean,
+    
+    PRIMARY KEY(id)
+) DEFAULT CHARSET=utf8; 
+
+CREATE TABLE IF NOT EXISTS Reserva (
+	cpfCli varchar(11) NOT NULL,
+    idQuarto INT NOT NULL,
+    data_ini DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    
+    PRIMARY KEY(cpfCli, idQuarto),
+    CONSTRAINT cliRef
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf),
+	CONSTRAINT id_quarto
+		FOREIGN KEY (idQuarto) REFERENCES Quarto(id)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Hospeda (
+	cpfCli varchar(11) NOT NULL,
+    idQuarto INT NOT NULL,
+    data_ini DATE NOT NULL,
+    data_fim DATE NOT NULL,
+    
+    PRIMARY KEY(cpfCli, idQuarto),
+    CONSTRAINT cli_ref
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf),
+	CONSTRAINT idQuarto
+		FOREIGN KEY (idQuarto) REFERENCES Quarto(id)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Equipamento(
+	quarto_id INT NOT NULL,
+    nome varchar(100) NOT NULL,
+    
+    PRIMARY KEY(quarto_id, nome),
+    CONSTRAINT quarto_id
+		FOREIGN KEY (quarto_id) REFERENCES Quarto(id) ON DELETE CASCADE
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Funcionario (
+	nome varchar(100) NOT NULL,
+    cpf varchar(11) NOT NULL,
+    salario real NOT NULL,
+    data_de_nascimento DATE NOT NULL,
+    
+    PRIMARY KEY(cpf)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Servico (
+	id INT NOT NULL,
+    valor real NOT NULL,
+    
+    PRIMARY KEY(id)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS Produto (
+	id INT NOT NULL,
+    nome varchar(100) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    valor real NOT NULL,
+    
+    PRIMARY KEY(id)
 ) DEFAULT CHARSET=utf8;

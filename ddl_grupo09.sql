e5750b701957b216254b932d4dc950b957ffe26f
manoelf <jose.ferreira@ccc.ufcg.edu.br>
2017-07-29 15:59:31 -0300

    Adicionando cheks e pronto pra rodar no apex

--- ddl_grupo09.sql
+++ ddl_grupo09.sql
@@ -1,27 +1,29 @@
 USE bd1;
 
-DROP TABLE IF EXISTS Avalia;
-DROP TABLE IF EXISTS Hospeda;
-DROP TABLE IF EXISTS Reserva;
-DROP TABLE IF EXISTS Telefones;
-DROP TABLE IF EXISTS Dependente;
-DROP TABLE IF EXISTS Nota_Fiscal;
-DROP TABLE IF EXISTS Cliente;
-DROP TABLE IF EXISTS Equipamento;
-DROP TABLE IF EXISTS Vende;
-DROP TABLE IF EXISTS Quarto;
-DROP TABLE IF EXISTS Prestados;
-DROP TABLE IF EXISTS Funcionario;
-DROP TABLE IF EXISTS Servico;
-DROP TABLE IF EXISTS Produto;
-DROP TABLE IF EXISTS ServicoLavanderia;
-DROP TABLE IF EXISTS ServicoPasseioTuristico;
-DROP TABLE IF EXISTS ServicoFrigobar;
-DROP TABLE IF EXISTS ServicoRestaurante;
-DROP TABLE IF EXISTS ServicoEstacionamento;
-DROP TABLE IF EXISTS ServicoBar;
+DROP TABLE Hospeda CASCADE CONSTRAINTS;
+DROP TABLE Reserva CASCADE CONSTRAINTS;
+DROP TABLE Telefones;
+DROP TABLE Dependente CASCADE CONSTRAINTS;
+DROP TABLE Nota_Fiscal CASCADE CONSTRAINTS;
+DROP TABLE Clientes CASCADE CONSTRAINTS;
+DROP TABLE Equipamento CASCADE CONSTRAINTS;
+DROP TABLE Quarto CASCADE CONSTRAINTS;
+DROP TABLE Funcionario CASCADE CONSTRAINTS;
+DROP TABLE Servico CASCADE CONSTRAINTS;
+DROP TABLE Produto CASCADE CONSTRAINTS;
+DROP TABLE ServicoLavanderia;
+DROP TABLE ServicoPasseioTuristico;
+DROP TABLE ServicoFrigobar;
+DROP TABLE ServicoRestaurante;
+DROP TABLE ServicoEstacionamento;
+DROP TABLE ServicoBar;
+DROP TABLE Prestados CASCADE CONSTRAINTS;
+DROP TABLE Avalia;
+DROP TABLE Vende CASCADE CONSTRAINTS;
 
-CREATE TABLE IF NOT EXISTS Cliente (
+
+
+CREATE TABLE  Clientes (
 	cpf char(11) PRIMARY KEY NOT NULL,
 	nome varchar(200) NOT NULL,
     email varchar(200) NOT NULL,
@@ -31,135 +33,145 @@ CREATE TABLE IF NOT EXISTS Cliente (
 	numero varchar(10) NOT NULL, 
 	bairro varchar(30) NOT NULL,
 	cep char(8) NOT NULL
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS Telefones (
+CREATE TABLE  Telefones (
 	cpfCli char(11) NOT NULL,
 	telefone varchar(9) NOT NULL,
 	
     PRIMARY KEY (cpfCli, telefone),
     CONSTRAINT cpfUsr
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE
+);
 
-CREATE TABLE IF NOT EXISTS Dependente(
-    cpf varchar(11) NOT NULL,
-    cpfCli varchar(11) NOT NULL,
+CREATE TABLE Dependente(
+    cpf char(11) NOT NULL,
+    cpfCli char(11) NOT NULL,
 	nome varchar(200) NOT NULL,
     dataNascimento DATE NOT NULL,
     
     PRIMARY KEY (cpf, cpfCli),
     CONSTRAINT cpfCliRef
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE
+); 
 
-CREATE TABLE IF NOT EXISTS Quarto (
+CREATE TABLE Quarto (
 	id INT NOT NULL,
-    numero INT NOT NULL UNIQUE,
+    numero integer NOT NULL UNIQUE,
     tipo varchar(100) NOT NULL,
-    vista boolean,
-    diaria real NOT NULL,
+    com_vista char(1),    
     
     PRIMARY KEY(id)
-) DEFAULT CHARSET=utf8; 
+);
 
-CREATE TABLE IF NOT EXISTS Reserva (
-	cpfCli varchar(11) NOT NULL,
+CREATE TABLE Reserva (
+	cpfCli char(11) NOT NULL,
     idQuarto INT NOT NULL,
     data_ini DATE NOT NULL,
     data_fim DATE NOT NULL,
     
     PRIMARY KEY(cpfCli, idQuarto),
     CONSTRAINT cliRef
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf),
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf),
 	CONSTRAINT id_quarto
-		FOREIGN KEY (idQuarto) REFERENCES Quarto(id)
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (idQuarto) REFERENCES Quarto(id),
+    CONSTRAINT check_data_reserva
+        CHECK(data_ini < data_fim)
+);
 
-CREATE TABLE IF NOT EXISTS Hospeda (
-	cpfCli varchar(11) NOT NULL,
+CREATE TABLE Hospeda (
+	cpfCli char(11) NOT NULL,
     idQuarto INT NOT NULL,
     data_ini DATE NOT NULL,
     data_fim DATE NOT NULL,
     
     PRIMARY KEY(cpfCli, idQuarto),
     CONSTRAINT cli_ref
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf),
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf),
 	CONSTRAINT idQuarto
-		FOREIGN KEY (idQuarto) REFERENCES Quarto(id)
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (idQuarto) REFERENCES Quarto(id),
+    CONSTRAINT check_data_hospedagem 
+        CHECK(data_ini < data_fim)
+);
 
-CREATE TABLE IF NOT EXISTS Nota_Fiscal(
+CREATE TABLE Nota_Fiscal(
 	id_nota INT NOT NULL,
     id_quarto INT NOT NULL,
-    cpfCli varchar(11) NOT NULL,
+    cpfCli char(11) NOT NULL,
     dt_in DATE NOT NULL,
     dt_out DATE NOT NULL,
     valor REAL NOT NULL,
     
     PRIMARY KEY (id_nota, cpfCli, id_quarto),
     CONSTRAINT cpfCliente
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf) ON DELETE CASCADE,
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE,
 	CONSTRAINT quartoRef
-		FOREIGN KEY (id_quarto) REFERENCES Quarto(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (id_quarto) REFERENCES Quarto(id) ON DELETE CASCADE,
+    CONSTRAINT check_duracao 
+        CHECK(dt_in < dt_out),
+    CONSTRAINT check_valor 
+        CHECK(valor >= 0)
+);
 
-CREATE TABLE IF NOT EXISTS Equipamento(
+CREATE TABLE Equipamento(
 	quarto_id INT NOT NULL,
     nome varchar(100) NOT NULL,
     
     PRIMARY KEY(quarto_id, nome),
     CONSTRAINT quarto_id
 		FOREIGN KEY (quarto_id) REFERENCES Quarto(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS Funcionario (
+CREATE TABLE Funcionario (
 	nome varchar(100) NOT NULL,
-    cpf varchar(11) NOT NULL,
+    cpf char(11) NOT NULL,
     salario real NOT NULL,
     data_de_nascimento DATE NOT NULL,
     
-    PRIMARY KEY(cpf)
-) DEFAULT CHARSET=utf8;
+    PRIMARY KEY(cpf),
+    CONSTRAINT check_salario
+        CHECK(salario > 0)
+);
 
-CREATE TABLE IF NOT EXISTS ServicoLavanderia (
+
+CREATE TABLE ServicoLavanderia (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS ServicoPasseioTuristico (
+CREATE TABLE ServicoPasseioTuristico (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS ServicoFrigobar (
+CREATE TABLE ServicoFrigobar (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS ServicoRestaurante (
+CREATE TABLE ServicoRestaurante (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS ServicoEstacionamento (
+CREATE TABLE ServicoEstacionamento (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS ServicoBar (
+CREATE TABLE ServicoBar (
 	id INT NOT NULL,
     
     PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8;
+);
 
 
-CREATE TABLE IF NOT EXISTS Servico (
+CREATE TABLE Servico (
 	id INT PRIMARY KEY NOT NULL,
     valor real NOT NULL,
     id_ser_lavanderia INT,
@@ -185,11 +197,42 @@ CREATE TABLE IF NOT EXISTS Servico (
 		FOREIGN KEY (id_ser_estacionamento) REFERENCES ServicoEstacionamento(id) ON DELETE CASCADE,
         
 	CONSTRAINT id_ser_bar
-		FOREIGN KEY (id_ser_bar) REFERENCES ServicoBar(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (id_ser_bar) REFERENCES ServicoBar(id) ON DELETE CASCADE,
+    CONSTRAINT check_valor_servico
+        CHECK (valor >= 0)
+);
 
-CREATE TABLE IF NOT EXISTS Prestados (
-	cpfFunc varchar(11) NOT NULL,
+
+
+CREATE TABLE Produto (
+	id INT NOT NULL,
+    nome varchar(100) NOT NULL,
+    descricao varchar(255) NOT NULL,
+    valor real NOT NULL,
+    
+    PRIMARY KEY(id),
+    CONSTRAINT check_valor_produto
+        CHECK (valor >= 0)
+);
+
+CREATE TABLE Avalia (
+	cpfCli char(11) NOT NULL,
+    idServico INT NOT NULL,
+    nota real NOT NULL,
+    comentario varchar(255),
+    
+    PRIMARY KEY(cpfCli, idServico),
+    CONSTRAINT cpf_cli
+		FOREIGN KEY (cpfCli) REFERENCES Clientes(cpf) ON DELETE CASCADE,
+	CONSTRAINT id_servico
+		FOREIGN KEY (idServico) REFERENCES Servico(id) ON DELETE CASCADE,
+    CONSTRAINT check_nota
+        CHECK (nota >= 0)
+);
+
+
+CREATE TABLE Prestados (
+	cpfFunc char(11) NOT NULL,
     idServico INT NOT NULL,
     
     PRIMARY KEY(cpfFunc, idServico),
@@ -197,31 +240,9 @@ CREATE TABLE IF NOT EXISTS Prestados (
 		FOREIGN KEY (cpfFunc) REFERENCES Funcionario(cpf) ON DELETE CASCADE,
 	CONSTRAINT idServico
 		FOREIGN KEY (idServico) REFERENCES Servico(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+);
 
-CREATE TABLE IF NOT EXISTS Avalia (
-	cpfCli varchar(11) NOT NULL,
-    idServico INT NOT NULL,
-    nota real NOT NULL,
-    comentario varchar(255),
-    
-    PRIMARY KEY(cpfCli, idServico),
-    CONSTRAINT cpf_cli
-		FOREIGN KEY (cpfCli) REFERENCES Cliente(cpf) ON DELETE CASCADE,
-	CONSTRAINT id_servico
-		FOREIGN KEY (idServico) REFERENCES Servico(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS Produto (
-	id INT NOT NULL,
-    nome varchar(100) NOT NULL,
-    descricao varchar(255) NOT NULL,
-    valor real NOT NULL,
-    
-    PRIMARY KEY(id)
-) DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS Vende (
+CREATE TABLE Vende (
 	idProduto INT NOT NULL,
     idQuarto INT NOT NULL,
     dataVenda DATE NOT NULL,
@@ -231,5 +252,7 @@ CREATE TABLE IF NOT EXISTS Vende (
     CONSTRAINT id_produto
 		FOREIGN KEY (idProduto) REFERENCES Produto(id) ON DELETE CASCADE,
 	CONSTRAINT id_quarto_ref
-		FOREIGN KEY (idQuarto) REFERENCES Quarto(id) ON DELETE CASCADE
-) DEFAULT CHARSET=utf8;
+		FOREIGN KEY (idQuarto) REFERENCES Quarto(id) ON DELETE CASCADE,
+    CONSTRAINT check_quantidade
+        CHECK(quantidade >= 0)
+);
